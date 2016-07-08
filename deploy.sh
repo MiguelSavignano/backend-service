449e1506a54d7dca148bb9816272e4abf5c60017
Miguel <migue.masx@gmail.com>
2016-07-08 17:18:47 +0200

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,34 @@
-./build
-# npm version patch
-# npm version minor
-# npm version major
-npm publish
+PS3='change version'
+options=("patch" "minor" "major")
+select opt in "${options[@]}"
+do
+  case $opt in
+    "patch")
+        ./build
+        npm version patch
+        npm publish
+        echo "OK"
+        ;;
+    "minor")
+        ./build
+        npm version minor
+        npm publish
+        echo "OK"
+        ;;
+    "major")
+        ./build
+        npm version major
+        npm publish
+        echo "OK"
+        ;;
+    "exit")
+        break
+        ;;
+    *) echo invalid option;;
+  esac
+done
+
+# ./build
+# # npm version minor
+# # npm version major
+# npm publish
